4b0803f8342d20320b1c7dbc24e2e2294db5c47a
zciw@me.com <zciw@me.com>
2022-12-22 12:01:16 +0100

    simple example

--- Program.cs
+++ Program.cs
@@ -1,12 +1,46 @@
 ﻿using System;
+using System.Threading;
 
 namespace thread
 {
     class Program
     {
-        static void Main(string[] args)
+        static void displayText()
+        {
+            Console.WriteLine("some text");
+        }
+        
+        static int[] generujTab(int rozmiar)
+        {
+            int[] t = new int[rozmiar];
+            Random r = new Random();
+            for (int i = 0; i < t.Length; i++)
+            {
+                t[i] = r.Next(1, 5);
+            }
+            return t;
+        }
+ 
+        static int suma(int[] t, int start, int end)
+        {
+            var n = 0;
+            for (int i = start; i < end; i++)
+            {
+                n = t[i] + n;
+            }
+            return n;
+        }
+		static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+			Thread t = new Thread(new ThreadStart(displayText));
+
+            t.Start();
+            Console.WriteLine("other text");
+
+            int[] losowe = generujTab(1000);
+            int s = suma(losowe, 0, losowe.Length);
+            Console.WriteLine(s);
         }
     }
 }
